3289ab89bdb9ec89e60a7a13dc73b0bacec7c142
云游君 <me@yunyoujun.cn>
2021-10-21 18:15:26 +0800

    Update element-plus.ts

--- plugins/element-plus.ts
+++ plugins/element-plus.ts
@@ -2,5 +2,5 @@ import { defineNuxtPlugin } from '#app'
 import * as ElementPlus from 'element-plus/lib'
 
 export default defineNuxtPlugin((nuxtApp: { vueApp: { use: (arg0: typeof ElementPlus) => void } }) => {
-    nuxtApp.vueApp.use(ElementPlus)
+  nuxtApp.vueApp.use(ElementPlus)
 })
