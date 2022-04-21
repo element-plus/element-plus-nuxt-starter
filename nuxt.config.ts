48f0b89d4224a7b18a8ed4cba72fcda37105e116
三咲智子 <sxzz@sxzz.moe>
2022-04-21 23:35:19 +0800

    chore: upgrade deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,33 @@
-import { defineNuxtConfig } from "nuxt3";
+import { defineNuxtConfig } from 'nuxt'
 
-const lifecycle = process.env.npm_lifecycle_event;
+const lifecycle = process.env.npm_lifecycle_event
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   // meta
   meta: {
-    title: "ElementPlus + Nuxt3",
+    title: 'ElementPlus + Nuxt3',
     meta: [
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: "description",
-        name: "description",
-        content: "ElementPlus + Nuxt3",
+        hid: 'description',
+        name: 'description',
+        content: 'ElementPlus + Nuxt3',
       },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // css
-  css: ["~/assets/scss/index.scss"],
+  css: ['~/assets/scss/index.scss'],
 
   // build
   build: {
-    transpile: lifecycle === "build" ? ["element-plus"] : [],
+    transpile: lifecycle === 'build' ? ['element-plus'] : [],
   },
 
   // build modules
-  buildModules: [
-    '@vueuse/nuxt',
-    '@unocss/nuxt',
-    "@pinia/nuxt",
-    "@nuxtjs/svg",
-  ],
+  modules: ['@vueuse/nuxt', '@unocss/nuxt', '@pinia/nuxt', '@nuxtjs/svg'],
 
   // auto import components
   components: true,
@@ -50,7 +45,10 @@ export default defineNuxtConfig({
       scale: 1.2,
     },
     shortcuts: [
-      ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+      [
+        'btn',
+        'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50',
+      ],
     ],
   },
-});
+})
